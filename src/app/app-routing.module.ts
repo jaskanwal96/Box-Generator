72a697f77a3aaf6846fb3d3d8dab2de93d3b7454
uTrade <utrade@uTrades-MacBook-Air.local>
2021-05-14 11:03:53 +0530

    Routing simplified for Heroku

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'box-generator', pathMatch: 'full'},
-    { path: 'box-generator', loadChildren: () => import('./box-generator/box-generator.module').then(m => m.BoxGeneratorModule), pathMatch: 'full'}
+    { path: '', loadChildren: () => import('./box-generator/box-generator.module').then(m => m.BoxGeneratorModule), pathMatch: 'full'}
 ];
 
 @NgModule({
